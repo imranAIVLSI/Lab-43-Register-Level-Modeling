e2eebbddafeba048ec7d92f5417d84fa737d46e2
Muhammad Imran <m.imranmani1571@gmail.com>
2025-07-09 12:47:43 +0500

    Register model created

---
+++ code/task1_rm_gen/reg_verifier_dir/uvmreg/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
@@ -0,0 +1,3 @@
+1752046038 /home/cc/Downloads/Imran/UVM/LABS/Lab-43-Register-Level-Modeling/code/task1_rm_gen/reg_verifier_dir/uvmreg/yapp_router_regs_rdb.sv
+1752046038 /home/cc/Downloads/Imran/UVM/LABS/Lab-43-Register-Level-Modeling/code/task1_rm_gen/reg_verifier_dir/uvmreg/cdns_uvmreg_utils_pkg.sv
+1752046461 /home/cc/Downloads/Imran/UVM/LABS/Lab-43-Register-Level-Modeling/code/task1_rm_gen/reg_verifier_dir/uvmreg/quicktest.sv
